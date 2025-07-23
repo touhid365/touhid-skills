d86556bf4d49399e55f96c2618a85df639ebb4e8
touhid365 <touhidanowar88@gmail.com>
2025-07-23 15:27:53 +0530

    hello

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,6 @@
 import Hero from '../components/Hero';
 import About from '../components/About';
 import Skills from '../components/Skills';
-import Services from '../components/Services';
 import Projects from '../components/Projects';
 import Contact from '../components/Contact';
 
